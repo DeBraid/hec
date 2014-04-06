727dc3dc61a62d89591a6ca28fa4949f74d5027c
DeBraid <de.braid@gmail.com>
2014-04-06 15:06:58 -0400

    sorting data: _.zip to merge two arrays

--- js/gdpchart.js
+++ js/gdpchart.js
@@ -1,10 +1,29 @@
 
-d3.csv('/../canGDP.csv', function (data) {
+// d3.csv('/../citygdp.csv', function (error, data) {
+  
+//   var nest = d3.nest()
+//     .key(function(d) { return d.Period; })
+//     .key(function(d) { return d.Hamilton; })
+//     .key(function(d) { return d.Toronto; })
+//     .entries(parsed);;  
+// })
 
+d3.json('/js/citiesgdp.json', function ( error, data ) { 
+  
 
-  console.log(data);
+  // console.log(data[0].Hamilton);
 
+  data.forEach(function (d){
+    
+    var headers = Object.keys(data[0]);
+    console.log(headers);
+
+    var arr = _.zip(d.Period, d.Toronto);
+    console.log(arr);
+
+    
+  
+  })
+
+  
 })
-
-
-d3.select('#chart-container').append('svg').attr({"width": 500, "height": 500}); 
