6d1560a11573394c53e784f744dd6f92b8c8d2f9
DeBraid <de.braid@gmail.com>
2014-04-06 16:18:52 -0400

    refactored: fetching gdp as object with labels

--- js/gdpchart.js
+++ js/gdpchart.js
@@ -1,38 +1,25 @@
-
-// d3.csv('/../citygdp.csv', function (error, data) {
-  
-//   var nest = d3.nest()
-//     .key(function(d) { return d.Period; })
-//     .key(function(d) { return d.Hamilton; })
-//     .key(function(d) { return d.Toronto; })
-//     .entries(parsed);;  
-// })
+var gdp = {};
 
 d3.json('/js/citiesgdp.json', function ( error, data ) { 
-  
-
-  // console.log(data[0].Hamilton);
-
-  data.forEach(function (d){
+  console.log(data);
+  console.log('data ^^ - ');
+  data.forEach(function ( d ) {
     
     var headers = Object.keys(data[0]);
 
     headers.forEach(function(header) {
-      var gdp = _.zip(d.Period, d[header]); 
-      console.log(gdp);
-      
-      
 
-      // var arrInv = _.invoke(d.Period, d.Toronto, 'sort');
-      // console.log(arrInv);
-      // console.log('arrInv - ');
-      
-      // var arrZip = _.zip(d.Period, d.Toronto);
-      // console.log(arrZip);
-      // console.log('arrZip - - ');
+      gdp[header] = _.zip(d.Period, d[header]); 
 
     })
+
+    return gdp;
+
   })
 
-  
+  console.log(gdp);
+  console.log(gdp.length);
+
+
 })
+
