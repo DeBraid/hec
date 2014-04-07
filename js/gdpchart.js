aa55232643c204bad4b854e83e2c83ddcb84d16e
DeBraid <de.braid@gmail.com>
2014-04-07 16:25:05 -0400

    new data (unemployment + Income), d3.json to import

--- js/gdpchart.js
+++ js/gdpchart.js
@@ -1,4 +1,7 @@
 var gdp = {};
+var income = {};
+var uRate = {};
+
 
 d3.json('/js/citiesgdp.json', function ( error, data ) { 
   
@@ -16,7 +19,52 @@ d3.json('/js/citiesgdp.json', function ( error, data ) {
 
   });
 
-  console.log(gdp);
+  var arrGdp = [gdp];
+  // console.log(arrGdp);
+
+});
+
+
+d3.json('/js/citiesIncome.json', function ( error, data ) { 
+  
+  data.forEach(function ( d ) {
+    var headers = Object.keys(data[0]);
+
+    headers.forEach(function ( header ) {
+      
+      income[header] = _.zip(d.Period, d[header]);
+
+    });
+  
+    return income;
+
+  });
+
+  var arrInc= [income];
+  
+
+});
+
+
+d3.json('/js/unemprate.json', function ( error, data ) { 
+  
+  data.forEach(function ( d ) {
+    var headers = Object.keys(data[0]);
+
+    headers.forEach(function ( header ) {
+      // zip combines 2 arrays by matching 
+      // values at same index position
+      uRate[header] = _.zip(d.Period, d[header]);
+
+    });
+  
+    return uRate;
+
+  });
+
+  var arrRate= [uRate];
+  console.log(arrRate);
+  console.log(arrRate.length);
 
 });
 
