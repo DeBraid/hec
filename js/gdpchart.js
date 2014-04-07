2bdff279656ffdb974a648110917719e82302f47
DeBraid <de.braid@gmail.com>
2014-04-07 10:37:29 -0400

    zip up arrays, output GDP as object

--- js/gdpchart.js
+++ js/gdpchart.js
@@ -1,25 +1,22 @@
 var gdp = {};
 
 d3.json('/js/citiesgdp.json', function ( error, data ) { 
-  console.log(data);
-  console.log('data ^^ - ');
+  
   data.forEach(function ( d ) {
-    
     var headers = Object.keys(data[0]);
 
-    headers.forEach(function(header) {
+    headers.forEach(function ( header ) {
+      // zip combines 2 arrays by matching 
+      // values at same index position
+      gdp[header] = _.zip(d.Period, d[header]);
 
-      gdp[header] = _.zip(d.Period, d[header]); 
-
-    })
-
+    });
+  
     return gdp;
 
-  })
+  });
 
   console.log(gdp);
-  console.log(gdp.length);
 
-
-})
+});
 
