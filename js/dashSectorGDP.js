ac7dd9cdb0e850b52a225938d2c7ea274f11774e
DeBraid <de.braid@gmail.com>
2014-05-26 11:02:12 -0400

    redraw function added, run w/o error, but no data update

--- js/dashSectorGDP.js
+++ js/dashSectorGDP.js
@@ -13,6 +13,8 @@ function makeChart ( error, data ) {
   var maxBarWidth = 480; // width of the bar with the max value
   var barnumber = 10;
   var page = 1;
+  var _next = document.getElementById('next');
+  var _last = document.getElementById('last');
   // accessor functions 
   var barLabel = function(d) { return d['type']; };
   var barValue = function(d) { return parseFloat(+d['gdp']); };
@@ -21,19 +23,19 @@ function makeChart ( error, data ) {
   var sortedData = data.sort(function(a, b) {
    return d3.descending(barValue(a), barValue(b));
   }); 
-
   var viewdata = sortedData.slice((page-1)*barnumber,page*barnumber);
-  // scales
+
+  // svg container element
+  var chart = d3.select('#mega-chart').append("svg")
+    .attr('width', maxBarWidth + barLabelWidth + valueLabelWidth)
+    .attr('height', gridLabelHeight + gridChartOffset + viewdata.length * barHeight); 
+
+ // scales
   var yScale = d3.scale.ordinal().domain(d3.range(0, viewdata.length)).rangeBands([0, viewdata.length * barHeight]);
   var y = function(d, i) { return yScale(i); };
   var yText = function(d, i) { return y(d, i) + yScale.rangeBand() / 2; };
   var x = d3.scale.linear().domain([0, d3.max(viewdata, barValue)]).range([0, maxBarWidth]);
   
-  // svg container element
-  var chart = d3.select('#mega-chart').append("svg")
-    .attr('width', maxBarWidth + barLabelWidth + valueLabelWidth)
-    .attr('height', gridLabelHeight + gridChartOffset + viewdata.length * barHeight);
-
 
   // grid line labels
   var gridContainer = chart.append('g')
@@ -100,4 +102,122 @@ function makeChart ( error, data ) {
     .attr("y2", yScale.rangeExtent()[1] + gridChartOffset)
     .style("stroke", "#000");
 
+  _next.onclick = function() {
+    page++;
+    viewdata = data.slice((page-1)*barnumber,page*barnumber);
+    redraw();
+  };
+
+
+  _last.onclick = function() {
+      page--;
+      viewdata = data.slice((page-1)*barnumber,page*barnumber);
+      redraw();
+  };
+  
+  function redraw () {
+  
+  /* Draw code begins */
+  console.log('redraw * init *');
+
+    // scales
+    var yScale = d3.scale.ordinal()
+        .domain(d3.range(0, viewdata.length))
+        .rangeBands([0, viewdata.length * barHeight]);
+
+
+    var y = function(d, i) { return yScale(i); };
+        console.log('redraw * yScale *');
+    var yText = function(d, i) { return y(d, i) + yScale.rangeBand() / 2; };
+    var x = d3.scale.linear()
+        .domain([0, d3.max(viewdata, barValue)])
+        .range([0, maxBarWidth]);
+        
+        console.log('redraw * xScale *');
+  
+
+    // grid line labels
+    var gridContainer = chart.append('g')
+      .attr('transform', 'translate(' + barLabelWidth + ',' + gridLabelHeight + ')'); 
+
+    gridContainer.selectAll("text")
+      .data(x.ticks(10)).transition().duration(500)
+      .attr("x", x)
+      .attr("dy", -3)
+      .attr("text-anchor", "middle")
+      .text(String);
+        console.log('redraw * gridContainer *');
+
+    // vertical grid lines
+    gridContainer.selectAll("line")
+      .data(x.ticks(10)).transition().duration(500)
+      .attr("x1", x)
+      .attr("x2", x)
+      .attr("y1", 0)
+      .attr("y2", yScale.rangeExtent()[1] + gridChartOffset)
+      .style("stroke", "#ccc");
+        
+        console.log('redraw * gridContainer 2 *');
+
+    // bar labels
+    var labelsContainer = chart.append('g')
+      .attr('transform', 'translate(' + (barLabelWidth - barLabelPadding) + ',' + (gridLabelHeight + gridChartOffset) + ')'); 
+    
+        console.log('redraw * labelContainer *');
+    
+    labelsContainer.selectAll('text')
+      .data(viewdata).transition().duration(500)
+      .attr('y', yText)
+      .attr('stroke', 'none')
+      .attr('fill', 'black')
+      .attr("dy", ".35em") // vertical-align: middle
+      .attr('text-anchor', 'end')
+      .text(barLabel);
+
+        console.log('redraw * labelContainer 2 *');
+
+
+    // bars
+    var barsContainer = chart.append('g')
+      .attr('transform', 'translate(' + barLabelWidth + ',' + (gridLabelHeight + gridChartOffset) + ')'); 
+
+    barsContainer.selectAll("rect")
+      .data(viewdata).transition().duration(500)
+      .attr('y', y)
+      .attr('height', yScale.rangeBand() - 9)
+      .attr('width', function ( d ) { return x(barValue(d)); })
+      .attr('stroke', 'white')
+      .attr("id", function ( d,i ) { return barLabel(d); })
+      .attr('fill','#00AE9D');
+
+    // bar value labels
+    barsContainer.selectAll("text")
+      .data(viewdata).transition().duration(500)
+      .attr("x", function(d) { return x(barValue(d)); })
+      .attr("y", yText)
+      .attr("dx", 3) // padding-left
+      .attr("dy", ".05em") // vertical-align: middle
+      .attr("text-anchor", "start") // text-align: right
+      .attr("fill", "black")
+      .attr("font-size", "15")
+      .attr("stroke", "none")
+      .text(function(d) { return d3.round(barValue(d), 2); });
+
+        console.log('redraw * barsContainer *');
+
+
+    // start line
+    barsContainer.append("line")
+      .attr("y1", -gridChartOffset)
+      .attr("y2", yScale.rangeExtent()[1] + gridChartOffset)
+      .style("stroke", "#000");
+
+        console.log('redraw * end *');
+
+
+  
+
+  };
+
+  console.log('foo');
 }
