8d82c4a44d6e28ad0306fce12655a31df80d4847
DeBraid <de.braid@gmail.com>
2014-06-01 12:09:08 -0400

    both charts will draw but SVG namespace issues prevents both from drawing at same time

--- js/allemp-ratio.js
+++ js/allemp-ratio.js
@@ -1,6 +1,6 @@
 var valueLabelWidth = 40; // space reserved for value labels (right)
 var barHeight = 36; // height of one bar
-var barLabelWidth = 200; // space reserved for bar labels
+var barLabelWidth = 10; // none for this chart  - space reserved for bar labels
 var barLabelPadding = 5; // padding between bar and bar labels (left)
 var gridLabelHeight = 18; // space reserved for gridline labels
 var gridChartOffset = 10; // space between start of grid and first bar
@@ -9,7 +9,7 @@ var totalEmp = 379200;
  
 // accessor functions 
 var barLabel = function(d) { return d['Company']; };
-var barValue = function(d) { return parseFloat(+d['Employees']); };
+var barValue = function(d) { return parseFloat(+d['Ratio']); };
 
 var tip = d3.tip()
     .attr('class', 'd3-tip')
@@ -29,20 +29,38 @@ d3.csv('/csv/allemp.csv', function (error, data) {
      return d3.descending(barValue(a), barValue(b));
     }); 
     
-    if (sortedData.length > 10) { sortedData.length = 12 };
 
+    var newData = sortedData.map(function (d) {
+
+      var rat = d3.round(d.Employees*100/totalEmp, 2);
+
+      return {
+                Company: d.Company,
+                Employees: +d.Employees,
+                Ratio: rat
+              }
+
+    })
+
+    newData.sort(function(a, b) {
+     return d3.descending(barValue(a), barValue(b));
+    }); 
+
+    if (newData.length > 12) { newData.length = 12 };
+
+    console.log(newData);
     // scales
-    var yScale = d3.scale.ordinal().domain(d3.range(0, sortedData.length)).rangeBands([0, sortedData.length * barHeight]);
+    var yScale = d3.scale.ordinal().domain(d3.range(0, newData.length)).rangeBands([0, newData.length * barHeight]);
     var y = function(d, i) { return yScale(i); };
     var yText = function(d, i) { return y(d, i) + yScale.rangeBand() / 2; };
-    var x = d3.scale.linear().domain([0, d3.max(sortedData, barValue)]).range([0, maxBarWidth]);
+    var x = d3.scale.linear().domain([0, d3.max(newData, barValue)]).range([0, maxBarWidth]);
     // svg container element
-    var chart = d3.select('#all-employees').append("svg")
+    var chart2 = d3.select('#ratio-employees').append("svg")
       .attr('width', maxBarWidth + barLabelWidth + valueLabelWidth)
-      .attr('height', gridLabelHeight + gridChartOffset + sortedData.length * barHeight);
+      .attr('height', gridLabelHeight + gridChartOffset + newData.length * barHeight);
 
     // grid line labels
-    var gridContainer = chart.append('g')
+    var gridContainer = chart2.append('g')
       .attr('transform', 'translate(' + barLabelWidth + ',' + gridLabelHeight + ')'); 
 
     gridContainer.selectAll("text")
@@ -62,24 +80,16 @@ d3.csv('/csv/allemp.csv', function (error, data) {
       .style("stroke", "#ccc");
 
     // bar labels
-    var labelsContainer = chart.append('g')
-      .attr('transform', 'translate(' + (barLabelWidth - barLabelPadding) + ',' + (gridLabelHeight + gridChartOffset) + ')'); 
-    labelsContainer.selectAll('text').data(sortedData).enter().append('text')
-      .attr('y', yText)
-      .attr('stroke', 'none')
-      .attr('fill', 'black')
-      .attr("dy", ".35em") // vertical-align: middle
-      .attr('text-anchor', 'end')
-      .text(barLabel);
+    //  none for this chart
 
     // bars
-    var barsContainer = chart.append('g')
+    var barsContainer = chart2.append('g')
       .attr('transform', 'translate(' + barLabelWidth + ',' + (gridLabelHeight + gridChartOffset) + ')'); 
 
-      chart.call(tip);
+      chart2.call(tip);
 
     barsContainer.selectAll("rect")
-      .data(sortedData).enter().append("rect")
+      .data(newData).enter().append("rect")
       .attr('y', y)
       .attr('height', yScale.rangeBand() - 9)
       .attr('width', function ( d ) { return x(barValue(d)); })
@@ -91,7 +101,7 @@ d3.csv('/csv/allemp.csv', function (error, data) {
 
     // bar value labels
     barsContainer.selectAll("text")
-      .data(sortedData).enter().append("text")
+      .data(newData).enter().append("text")
       .attr("x", function(d) { return x(barValue(d)); })
       .attr("y", yText)
       .attr("dx", 3) // padding-left
