70e1b7f1a2d2a026a82d024772f0758863633fb7
DeBraid <de.braid@gmail.com>
2014-06-04 09:31:23 -0400

    cleaned up main wealth.js and moved buttons down with css rather than hackhtml

--- js/wealth.js
+++ js/wealth.js
@@ -102,12 +102,6 @@ d3.json("/js/myNations.json", function(nations) {
       .call(position)
       .sort(order);
 
-  var hammer = d3.select("circle#Hamilton.dot"); 
-
-  hammer.style({
-    "fill": "url(#image)"
-  });
-
   // Add an overlay for the year label.
   var box = label.node().getBBox();
 
